0babf5764efab7f943cadc872cacf8e715468ffa
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2019-02-03 21:46:55 +0200

    tiv: depend on imagemagick

--- Formula/tiv.rb
+++ Formula/tiv.rb
@@ -1,16 +1,18 @@
 class Tiv < Formula
-  desc "Small C++ program to display images in a (modern) terminal using RGB ANSI codes and unicode block graphics characters"
+  desc "Display images in a terminal"
   homepage "https://github.com/stefanhaustein/TerminalImageViewer"
   url "https://github.com/stefanhaustein/TerminalImageViewer/archive/v1.0.0.tar.gz"
   sha256 "d28c5746d25d83ea707db52b54288c4fc1851c642ae021951967e69296450c8c"
+
+  depends_on "imagemagick"
+
+  fails_with :clang do
+    cause "experimental/filesystem missing"
+  end
 
   def install
     chdir "src/main/cpp"
     system "make", "CXX=#{ENV.cxx}"
     bin.install "tiv"
-  end
-
-  fails_with :clang do
-    cause "experimental/filesystem missing"
   end
 end
